7adced3b132c0f4e5be001220a06588262fee02a
Richard Orchard <richard.orchard@relationshipswa.org.au>
2026-06-12 12:23:23 +0800

    trying to get function to work

--- api/Program.cs
+++ api/Program.cs
@@ -1,9 +1,7 @@
-using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
+    .ConfigureFunctionsWorkerDefaults()
     .Build();
 
 host.Run();
